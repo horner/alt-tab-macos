1d4fceae07894a6c9626a0e1ea4eec340cc2ebbb
Doug Horner <horner@mieweb.com>
2026-09-23 04:36:19 -0400

    chore: release AltTabProjects 0.1.2

--- Casks/alttab-projects.rb
+++ Casks/alttab-projects.rb
@@ -1,8 +1,8 @@
 cask "alttab-projects" do
-  version "0.1.1"
-  sha256 "92851b49fdaef0d0d0c0670c0b431cc8d9ed130cd90d2ca03caa341f8ad21f59"
+  version "0.1.2"
+  sha256 "8d7977ab3bf95c9a0f58c3cb786990c401b6702231fa8296479f4b7609446bcf"
 
-  url "https://github.com/horner/alt-tab-macos/releases/download/projects-v0.1.1/AltTabProjects.zip"
+  url "https://github.com/horner/alt-tab-macos/releases/download/projects-v0.1.2/AltTabProjects.zip"
   name "AltTabProjects"
   desc "Window and Desktop switcher with saved Projects"
   homepage "https://github.com/horner/alt-tab-macos"
